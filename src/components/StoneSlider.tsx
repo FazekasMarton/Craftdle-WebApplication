b7fd852c793e05c30a8f86d65ef9b591be076fb3
FazekasMarton <fazekas.marton16@gmail.com>
2024-11-28 16:00:41 +0100

    General settings finished

--- src/components/StoneSlider.tsx
+++ src/components/StoneSlider.tsx
@@ -1,11 +1,17 @@
+import { ISettings } from "../interfaces/ISettings"
+
 interface StoneSliderProps{
     min: number,
     max: number,
-    value: number
+    value: number,
+    setValue: {
+        fun: <K extends keyof ISettings>(key: K, value: ISettings[K]) => void,
+        key: keyof ISettings
+    }
 }
 
 export function StoneSlider(props: StoneSliderProps){
     return <div className="stoneSlider">
-        <input type="range" min={props.min} max={props.max} value={props.value} />
+        <input type="range" min={props.min} max={props.max} value={props.value} onChange={(e) => {props.setValue.fun(props.setValue.key, Number(e.currentTarget.value))}}/>
     </div>
 }
--- src/pages/Settings/SettingsMain.tsx
+++ src/pages/Settings/SettingsMain.tsx
@@ -2,47 +2,70 @@ import { StoneButton } from "../../components/StoneButton"
 import { StoneSlider } from "../../components/StoneSlider"
 import { ISettings } from "../../interfaces/ISettings"
 
-function* generateTableMapping(table: Array<string>) {
-    for (let i = 1; i <= 9; i++) {
-        yield
+interface SettingsMainProps {
+    profile: number;
+    profiles: Array<ISettings>;
+    setSettings: (value: Array<ISettings>) => void
+}
+
+const defaultSettings: ISettings = {
+    id: 0,
+    isSet: false,
+    volume: 50,
+    imagesSize: 50,
+    controls: {
+        isTapMode: false,
+        copy: "Left Mouse Click",
+        remove: "Right Mouse Click",
+        teableMapping: ["1", "2", "3", "4", "5", "6", "7", "8", "9"]
     }
 }
 
-interface SettingsMainProps {
-    profile: ISettings
-}
-
 export function SettingsMain(props: SettingsMainProps) {
+    const profile = props.profiles[props.profile]
+
+    function changeSettings<K extends keyof ISettings>(key: K, value: ISettings[K]) {
+        let newSettings: Array<ISettings> = JSON.parse(JSON.stringify(props.profiles))
+        newSettings[props.profile][key] = value
+        props.setSettings(newSettings)
+    }
+
     return <section id="settingsMain">
         <div id="settingsList">
             <h2 className="settingsLabel">General</h2>
 
             <span>Volume</span>
-            <StoneSlider min={0} max={100} value={props.profile.volume} />
-            <StoneButton>Reset</StoneButton>
+            <StoneSlider min={0} max={100} value={profile.volume} setValue={{ fun: changeSettings, key: "volume" }} />
+            <StoneButton
+                disabled={profile.volume == defaultSettings.volume}
+                onClick={() => { changeSettings("volume", defaultSettings.volume) }}
+            >Reset</StoneButton>
 
             <span>Images Size</span>
-            <StoneSlider min={0} max={100} value={props.profile.imagesSize} />
-            <StoneButton>Reset</StoneButton>
+            <StoneSlider min={0} max={100} value={profile.imagesSize} setValue={{ fun: changeSettings, key: "imagesSize" }} />
+            <StoneButton
+                disabled={profile.imagesSize == defaultSettings.imagesSize}
+                onClick={() => { changeSettings("imagesSize", defaultSettings.imagesSize) }}
+            >Reset</StoneButton>
 
             <h2 className="settingsLabel">Controls</h2>
 
             <span>Enable Tap Mode</span>
-            <StoneButton>{props.profile.controls.isTapMode ? "Enable" : "Disable"}</StoneButton>
+            <StoneButton>{profile.controls.isTapMode ? "Enable" : "Disable"}</StoneButton>
             <StoneButton>Reset</StoneButton>
 
             <span>Drag / Duplicate Items</span>
-            <StoneButton>{props.profile.controls.copy}</StoneButton>
+            <StoneButton>{profile.controls.copy}</StoneButton>
             <StoneButton>Reset</StoneButton>
 
             <span>Remove Items</span>
-            <StoneButton>{props.profile.controls.remove}</StoneButton>
+            <StoneButton>{profile.controls.remove}</StoneButton>
             <StoneButton>Reset</StoneButton>
 
             {Array.from({ length: 9 }, (_, i) => {
                 return <>
                     <span>Slot {i + 1}</span>
-                    <StoneButton>{props.profile.controls.teableMapping[i]}</StoneButton>
+                    <StoneButton>{profile.controls.teableMapping[i]}</StoneButton>
                     <StoneButton>Reset</StoneButton>
                 </>
             })}
