6a5114a41f15b11a4336aea4ffa57474f84970fe
HorvathMate04 <culubpenguin@gmail.com>
2024-12-14 02:27:04 +0100

    small fixes

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,12 @@ import { Settings } from "./pages/Settings/Settings"
 import { Info } from "./components/Info"
 import { Gamemodes } from "./pages/Gamemodes/Gamemodes"
 import { Game } from "./pages/Game/Game"
+import { useEffect } from "react"
+import { useSelector } from "react-redux"
+import { RootState, store } from "./app/store"
+import { loadUser, saveUser } from "./features/user/userSlice"
+import { guestLogin, tokenLogin } from "./features/user/dataRequestSlice"
+import { loadSettings } from "./functions/loadSettings"
 
 const router = createBrowserRouter([
     {
@@ -30,7 +36,39 @@ const router = createBrowserRouter([
     },
 ])
 
+async function autoLogin(token: string | null){
+    let error = true
+    if (token) {
+        let response = await store.dispatch(tokenLogin())
+        let res = (response.payload as any)
+        if (res.response) {
+            await store.dispatch(saveUser(res.data.data))
+            error = false
+        }
+    }
+    if(error){
+        let response = await store.dispatch(guestLogin())
+        let res = (response.payload as any)
+        store.dispatch(saveUser(res.data.data))
+    }
+    await loadSettings()
+}
+
 function App() {
+    const user = useSelector((state: RootState) => state.user);
+    
+    async function loadSavedUser() {
+        await store.dispatch(loadUser());
+        const token = store.getState().user.loginToken;
+        await autoLogin(token);
+    }
+
+    useEffect(() => {
+        if(!user.username){
+            loadSavedUser()
+        }
+    }, [])
+
     return (
         <>
             <RouterProvider router={router} />
