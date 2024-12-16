1b0b4acff01f7e0daf2ce04d648ddb52fa08f1fe
FazekasMarton <fazekas.marton16@gmail.com>
2024-12-16 11:54:10 +0100

    craft base added

--- src/pages/Game/CraftingTable.tsx
+++ src/pages/Game/CraftingTable.tsx
@@ -1,13 +1,23 @@
+import { useEffect, useState } from "react"
 import arrow from "../../assets/imgs/icons/arrow.png"
 import craftingBook from "../../assets/imgs/icons/recipe_book.png"
 import { Item } from "./Item"
+import { craft } from "../../functions/craft"
+import { IRecipeCollection } from "../../interfaces/IRecipe"
 
 interface CraftingTableProps {
     size: 2 | 3,
-    craftingTable: Array<Array<HTMLImageElement | null>>
+    craftingTable: Array<Array<HTMLImageElement | null>>,
+    recipes: IRecipeCollection
 }
 
 export function CraftingTable(props: CraftingTableProps) {
+    const [craftedItem, setCraftedItem] = useState<HTMLImageElement | null>(null)
+
+    useEffect(() => {
+        setCraftedItem(craft(props.craftingTable))
+    }, [props.craftingTable])
+
     return <div id="craftingTable">
         <h1 id="craftingTitle">Crafting</h1>
         <table>
@@ -18,7 +28,7 @@ export function CraftingTable(props: CraftingTableProps) {
                             const item = props.craftingTable[rowIndex]?.[slotIndex]
                             item?.classList.remove("item")
                             return <td key={slotIndex} id={`slot${rowIndex * 3 + slotIndex}`} className="slot">
-                                {item ? <Item item={item} className="item"/> : null}
+                                {item ? <Item item={item} className="item" /> : null}
                             </td>
                         })}
                     </tr>
@@ -26,7 +36,9 @@ export function CraftingTable(props: CraftingTableProps) {
             </tbody>
         </table>
         <img id="craftingArrow" src={arrow} alt="arrow" />
-        <div id="craftedItem" className="slot"></div>
+        <div id="craftedItem" className="slot">
+            {craftedItem ? <Item item={craftedItem}/> : null}
+        </div>
         <div id="craftingBook" className="slotButton">
             <img src={craftingBook} alt="Crafting Book" />
         </div>
