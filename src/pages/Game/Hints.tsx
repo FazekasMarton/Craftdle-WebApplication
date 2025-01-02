0a63a70abb5202970b7278a7a882dd0ec942936b
FazekasMarton <fazekas.marton16@gmail.com>
2025-01-02 13:21:32 +0100

    hints styled

--- src/pages/Game/Hints.tsx
+++ src/pages/Game/Hints.tsx
@@ -9,22 +9,30 @@ interface HintsProps {
 function HintButton(hint: string | null, index: number, turn: number) {
     const [showHint, setShowHint] = useState(false);
     let countDown = (index + 1) * 5 - turn;
-    let countDownText = countDown > 0 ? `Hint after ${countDown} turn${countDown == 1 ? "" : "s"}!` : "Revail hint!";
+    let countDownText = countDown > 0 ? (
+        `Hint after ${countDown} turn${countDown == 1 ? "" : "s"}!`
+    ) : "Revail hint!";
+
     return showHint ? (
         <div className="hintContainer">
             <div className="hintContent">{hint}</div>
-        </div> 
-    ):(
-        <Button color="green" onClick={() => { if(countDown <= 0) setShowHint(true) }}>{countDownText}</Button>
+        </div>
+    ) : (
+        <Button color="green" onClick={() => { if (countDown <= 0) setShowHint(true) }}>{countDownText}</Button>
     )
 }
 
 export function Hints(props: HintsProps) {
     return <div id="hints">
-        {
-            props.hints.map((hint, index) => {
-                return HintButton(hint, index, props.turn);
-            })
-        }
+        <h1 id="hintsTitle">Hints:</h1>
+        <div id="hintsContainer">
+            {
+                props.hints.map((hint, index) => {
+                    return <div className="hintSlot">
+                        {HintButton(hint, index, props.turn)}
+                    </div>
+                })
+            }
+        </div>
     </div>
 }
