55164e422ac6f463d2d4fa95aeda28242e958cf3
FazekasMarton <fazekas.marton16@gmail.com>
2025-01-08 18:31:50 +0100

    achievement finished

--- src/classes/Audio.ts
+++ src/classes/Audio.ts
@@ -1,13 +1,20 @@
-import clickSound from "../assets/audios/click.mp3"
+import clickSound from "../assets/audios/click.mp3";
+import achievementSound from "../assets/audios/Achievement.mp3";
 
-const clickAudio = new Audio(clickSound)
-clickAudio.preload = "auto"
+const clickAudio = new Audio(clickSound);
+clickAudio.preload = "auto";
 
-export class SoundEffect{
-    static play(effect: "click"){
+const achievementAudio = new Audio(achievementSound);
+achievementAudio.preload = "auto";
+
+export class SoundEffect {
+    static play(effect: "click" | "achievement") {
         switch (effect) {
             case "click":
-                new Audio(clickSound).play()
+                new Audio(clickSound).play();
+                break;
+            case "achievement":
+                new Audio(achievementSound).play();
                 break;
         }
     }
