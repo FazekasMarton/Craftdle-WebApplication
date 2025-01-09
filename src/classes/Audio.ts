e2a5a0148b3ccba91b93789b36f80d737ee51116
FazekasMarton <fazekas.marton16@gmail.com>
2025-01-09 22:31:50 +0100

    Credits page added

--- src/classes/Audio.ts
+++ src/classes/Audio.ts
@@ -1,5 +1,6 @@
 import clickSound from "../assets/audios/click.mp3";
 import achievementSound from "../assets/audios/Achievement.mp3";
+import creeperSound from "../assets/audios/creeper.mp3";
 
 const clickAudio = new Audio(clickSound);
 clickAudio.preload = "auto";
@@ -7,14 +8,20 @@ clickAudio.preload = "auto";
 const achievementAudio = new Audio(achievementSound);
 achievementAudio.preload = "auto";
 
+const creeperAudio = new Audio(creeperSound);
+creeperAudio.preload = "auto";
+
 export class SoundEffect {
-    static play(effect: "click" | "achievement") {
+    static play(effect: "click" | "achievement" | "creeper") {
         switch (effect) {
             case "click":
                 new Audio(clickSound).play();
                 break;
             case "achievement":
                 new Audio(achievementSound).play();
+                break;
+            case "creeper":
+                new Audio(creeperSound).play();
                 break;
         }
     }
