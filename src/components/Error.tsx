1745aa933ce8722fa8142dd714e43e86992f3c3c
FazekasMarton <fazekas.marton16@gmail.com>
2025-01-12 16:34:25 +0100

    Error component added to app; reload function added to Error

--- src/components/Error.tsx
+++ src/components/Error.tsx
@@ -1,9 +1,33 @@
 import { useSelector } from "react-redux";
-import { RootState } from "../app/store";
+import { RootState, store } from "../app/store";
 import { Button } from "./Button";
+import { useLocation, useNavigate } from "react-router-dom";
+import { resetError } from "../features/error/errorSlice";
 
 export function Error() {
     const error = useSelector((state: RootState) => state.error)
+    const navigate = useNavigate();
+    const location = useLocation();
+
+    async function reload() {
+        const urlParams = new URLSearchParams(location.search);
+
+        if(urlParams.get('newGame') != null){
+            urlParams.set('newGame', 'false');
+        }
+    
+        await store.dispatch(resetError());
+    
+        navigate({
+            pathname: location.pathname,
+            search: urlParams.toString(),
+        });
+    
+        window.location.reload();
+    }
+    
+
+
     return error.status != 200 ? (
         <div id="errorBorder">
             <div id='error'>
@@ -11,7 +35,7 @@ export function Error() {
                     <h1 id='errorCode'>{error.status}</h1>
                     <h2 id='errorTitle'>{error.name}</h2>
                     <p id='errorText'>{error.message}</p>
-                    <Button color="green">Try again</Button>
+                    <Button color="green" onClick={reload}>Try again</Button>
                 </div>
             </div>
         </div>
