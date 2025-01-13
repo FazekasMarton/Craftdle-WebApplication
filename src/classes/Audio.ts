0b1ba0231d898dc587bf81e8ce81c826944c0793
FazekasMarton <fazekas.marton16@gmail.com>
2025-01-13 10:01:09 +0100

    Audio class updated and volume setter added to it

--- src/classes/Audio.ts
+++ src/classes/Audio.ts
@@ -1,28 +1,21 @@
 import clickSound from "../assets/audios/click.mp3";
 import achievementSound from "../assets/audios/Achievement.mp3";
 import creeperSound from "../assets/audios/creeper.mp3";
+import { store } from "../app/store";
 
-const clickAudio = new Audio(clickSound);
-clickAudio.preload = "auto";
+const soundEffects = {
+    "click": new Audio(clickSound),
+    "achievement": new Audio(achievementSound),
+    "creeper": new Audio(creeperSound)
+} as const;
 
-const achievementAudio = new Audio(achievementSound);
-achievementAudio.preload = "auto";
-
-const creeperAudio = new Audio(creeperSound);
-creeperAudio.preload = "auto";
+Object.values(soundEffects).forEach(audio => audio.preload = "auto");
 
 export class SoundEffect {
-    static play(effect: "click" | "achievement" | "creeper") {
-        switch (effect) {
-            case "click":
-                new Audio(clickSound).play();
-                break;
-            case "achievement":
-                new Audio(achievementSound).play();
-                break;
-            case "creeper":
-                new Audio(creeperSound).play();
-                break;
-        }
+    static play(effect: keyof typeof soundEffects) {
+        const volume = store.getState().user.settings?.find(s => s.isSet)?.volume ?? 50;
+        const audio = new Audio(soundEffects[effect].src);
+        audio.volume = Math.max(0, Math.min(1, volume / 100));
+        audio.play();
     }
 }
