bba02156b3c4286516dd1e6ee369b8c0fdaec4f3
FazekasMarton <fazekas.marton16@gmail.com>
2025-01-19 12:58:10 +0100

    Profile Pics changer added to Collection

--- src/pages/Collection/Collection.tsx
+++ src/pages/Collection/Collection.tsx
@@ -1,9 +1,12 @@
 import { useEffect, useState } from "react";
 import { store } from "../../app/store";
-import { getCollection } from "../../features/user/dataRequestSlice";
+import { changeProfilePics, getCollection } from "../../features/user/dataRequestSlice";
 import { StoneButton } from "../../components/StoneButton";
 import { Achievement } from "../../components/Achievement";
+import active from "../../assets/imgs/icons/checkmark.png";
+import lock from "../../assets/imgs/icons/lock.png";
 import xp from "../../assets/imgs/backgrounds/experience_bar_progress.png";
+import xpBar from "../../assets/imgs/backgrounds/experience_bar_background.png";
 
 interface ICollection {
     inventory: Array<{
@@ -17,12 +20,14 @@ interface ICollection {
         name: string;
         src: string;
         collected: boolean;
+        active: boolean;
     }>;
     profileBorder: Array<{
         id: number;
         name: string;
         src: string;
         collected: boolean;
+        active: boolean;
     }>;
     achievement: Array<{
         title: string;
@@ -32,6 +37,14 @@ interface ICollection {
         goal: number;
         rarity: number;
     }>;
+}
+
+function saveProfileChanges(collection: ICollection, setCollection: (value: ICollection) => void) {
+    store.dispatch(changeProfilePics({
+        profilePicture: collection.profilePicture.find((item) => item.active)?.id || 0,
+        profileBorder: collection.profileBorder.find((item) => item.active)?.id || 0
+    }))
+    setCollection(collection)
 }
 
 export function Collection() {
@@ -49,8 +62,6 @@ export function Collection() {
         getUserCollection()
     }, []);
 
-    console.log(collection)
-
     return <div id="collection">
         <header id="collectionHeader">
             <nav>
@@ -65,8 +76,30 @@ export function Collection() {
                     <article>
                         {
                             collection?.profilePicture.map((item, index) => {
-                                return <div className={`itemFrame ${item.collected ? "" : "uncollectedItem"}`} key={index}>
+                                return <div
+                                    className={`itemFrame ${item.collected ? "" : "uncollectedItem"}`}
+                                    key={index}
+                                    onClick={() => {
+                                        if (item.collected && !item.active) {
+                                            collection.profilePicture.forEach((element) => {
+                                                element.active = element.id === item.id
+                                            })
+                                            saveProfileChanges({ ...collection }, setCollection)
+                                        }
+                                    }}
+                                >
                                     <img src={`http://localhost:3000/profilePictures/${item.src}`} alt={item.name} draggable={false} />
+                                    {
+                                        item.collected ? (
+                                            item.active ? (
+                                                <img className="activeProfileImage" src={active} alt="Active" />
+                                            ) : (
+                                                <img className="activeableProfileImage" src={active} alt="Active" />
+                                            )
+                                        ) : (
+                                            <img className="uncollectedProfileImage" src={lock} alt="Uncollected" />
+                                        )
+                                    }
                                 </div>
                             })
                         }
@@ -79,6 +112,17 @@ export function Collection() {
                             collection?.profileBorder.map((item, index) => {
                                 return <div className={`itemFrame ${item.collected ? "" : "uncollectedItem"}`} key={index}>
                                     <img src={`http://localhost:3000/profileBorders/${item.src}`} alt={item.name} draggable={false} />
+                                    {
+                                        item.collected ? (
+                                            item.active ? (
+                                                <img className="activeProfileImage" src={active} alt="Active" />
+                                            ) : (
+                                                <img className="activeableProfileImage" src={active} alt="Active" />
+                                            )
+                                        ) : (
+                                            <img className="uncollectedProfileImage" src={lock} alt="Uncollected" />
+                                        )
+                                    }
                                 </div>
                             })
                         }
@@ -91,6 +135,13 @@ export function Collection() {
                             collection?.inventory.map((item, index) => {
                                 return <div className={`itemFrame ${item.collected ? "" : "uncollectedItem"}`} key={index}>
                                     <img src={`http://localhost:3000/items/${item.src}`} alt={item.name} draggable={false} />
+                                    {
+                                        !item.collected ? (
+                                            <img className="uncollectedProfileImage" src={lock} alt="Uncollected" />
+                                        ) : (
+                                            null
+                                        )
+                                    }
                                 </div>
                             })
                         }
@@ -104,9 +155,10 @@ export function Collection() {
                                 return <div className={achievement.goal === achievement.progress ? "" : "uncollectedItem"} key={index}>
                                     <Achievement key={index} achievement={achievement} />
                                     <div className="progressBar">
-                                        <img className="xp" src={xp} alt="xp" style={{
+                                        <img className="xpBar" src={xpBar} alt="XP bar" />
+                                        <img className="xp" src={xp} alt="XP" style={{
                                             clipPath: `inset(0 ${100 - (achievement.progress / achievement.goal) * 100}% 0 0)`
-                                        }}/>
+                                        }} />
                                     </div>
                                 </div>
                             })
