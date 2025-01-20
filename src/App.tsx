9566ef6d8e82e10a43ef54d0de1e7326cca53b2b
FazekasMarton <fazekas.marton16@gmail.com>
2025-01-20 19:01:04 +0100

    Icon, title, description and keywords added to every page

--- src/App.tsx
+++ src/App.tsx
@@ -21,20 +21,40 @@ import { Docs } from "./pages/Docs/Docs"
 import { Error } from "./components/Error"
 import { isUserPlayingOnPC } from "./functions/isUserPlayingOnPC"
 import { Guide } from "./pages/Guide/Guide"
+import { Meta } from "./components/Meta"
+import { Collection } from "./pages/Collection/Collection"
+import { Stats } from "./pages/Stats/Stats"
 
 const normalRouter = createBrowserRouter([
     {
         path: "/",
-        element: <MainMenu />,
+        element: <>
+            <Meta />
+            <MainMenu />
+        </>,
         index: true
     },
     {
         path: "settings",
-        element: <Settings />
+        element: <>
+            <Meta
+                title="Settings"
+                description="Customize your game settings in Craftdle, including controls, audio, and icon sizes for a personalized experience on PC or mobile."
+                keywords="Craftdle, Game Settings, Controls, Audio, Icon Size, Customize Appearance, Game Preferences, Minecraft Puzzle Settings, Guideian Angel"
+            />
+            <Settings />
+        </>
     },
     {
         path: "singleplayer",
-        element: <Gamemodes type="singleplayer" />
+        element: <>
+            <Meta
+                title="Singleplayer"
+                description="Explore the singleplayer gamemodes in Craftdle! Test your crafting knowledge in unique puzzles designed for solo play. Play now on PC or mobile."
+                keywords="Minecraft, Wordle, Singleplayer, Craftdle Singleplayer, Solo Minecraft Puzzles, Crafting Challenges, Guideian Angel, Minecraft Wordle"
+            />
+            <Gamemodes type="singleplayer" />
+        </>
     },
     {
         path: "play",
@@ -42,19 +62,69 @@ const normalRouter = createBrowserRouter([
     },
     {
         path: "patchNotes",
-        element: <PatchNotes />
+        element: <>
+            <Meta
+                title="Patch Notes"
+                description="Stay up-to-date with the latest updates in Craftdle! Read the patch notes to learn about new features, bug fixes, and improvements in the game."
+                keywords="Craftdle, Patch Notes, Updates, Game Changes, New Features, Bug Fixes, Game Improvements, Guideian Angel"
+            />
+            <PatchNotes />
+        </>
     },
     {
         path: "credits",
-        element: <Credits />
+        element: <>
+            <Meta
+                title="Credits"
+                description="Meet the team behind Craftdle! Learn about the developers, artists, and contributors who helped create the game. Play now on PC or mobile."
+                keywords="Craftdle, Credits, Developers, Artists, Contributors, Game Creators, Game Designers, Guideian Angel"
+            />
+            <Credits />
+        </>
     },
     {
         path: "docs",
-        element: <Docs />
+        element: <>
+            <Meta
+                title="Privacy Policy & Terms of Use"
+                description="Review Craftdle's Privacy Policy and Terms of Use. Learn how we protect your data and the rules you agree to when using our game."
+                keywords="Craftdle, Privacy Policy, Terms of Use, Game Rules, Data Protection, User Agreement, Minecraft Puzzle Game, Guideian Angel"
+            />
+            <Docs />
+        </>
     },
     {
         path: "guide",
-        element: <Guide />
+        element: <>
+            <Meta
+                title="How to Play"
+                description="Learn how to play Craftdle! Read the guide to understand the game mechanics, controls, and strategies for solving puzzles. Play now on PC or mobile."
+                keywords="Craftdle, How to Play, Game Guide, Game Mechanics, Controls, Strategies, Puzzle Solving, Crafting Game, Guideian Angel"
+            />
+            <Guide />
+        </>
+    },
+    {
+        path: "collection",
+        element: <>
+            <Meta
+                title="Collection"
+                description="View your collection in Craftdle! Track your progress, view your achievements, and explore the puzzles you've completed. Play now on PC or mobile."
+                keywords="Craftdle, Collection, Achievements, Progress, Puzzles, Crafting Game, Puzzle Game, Guideian Angel"
+            />
+            <Collection />
+        </>
+    },
+    {
+        path: "stats",
+        element: <>
+            <Meta
+                title="Player Stats and Achievements - Craftdle"
+                description="Explore your personal game statistics, including your achievements, progress in different gamemodes, high scores, and overall performance in Craftdle."
+                keywords="Craftdle, Player Stats, Achievements, Game Progress, High Scores, Player Performance, Craftdle Achievements, Game Stats, Milestones, Puzzle Game Performance, Crafting Game Stats, Guideian Angel"
+            />
+            <Stats />
+        </>
     },
     {
         path: "*",
@@ -65,7 +135,14 @@ const normalRouter = createBrowserRouter([
 const maintenanceRouter = createBrowserRouter([
     {
         path: "/",
-        element: <Maintenance />,
+        element: <>
+            <Meta
+                title="Maintenance"
+                description="Craftdle is currently undergoing maintenance. Please check back later for updates. We apologize for any inconvenience."
+                keywords="Craftdle, Maintenance, Game Updates, Game Improvements, Game Downtime, Game Changes, Guideian Angel"
+            />
+            <Maintenance />
+        </>,
         index: true
     },
     {
--- src/main.tsx
+++ src/main.tsx
@@ -3,9 +3,12 @@ import './style.css'
 import App from './App.tsx'
 import { Provider } from 'react-redux'
 import { store } from './app/store.ts'
+import { HelmetProvider } from 'react-helmet-async'
 
 createRoot(document.getElementById('wrapper')!).render(
     <Provider store={store}>
-        <App />
+        <HelmetProvider>
+            <App />
+        </HelmetProvider>
     </Provider>
 )
