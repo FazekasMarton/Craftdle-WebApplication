da94ade1b32dfa251daf13dd2a59f47f97e43f3d
FazekasMarton <fazekas.marton16@gmail.com>
2025-01-22 10:51:48 +0100

    Tutorial text coloring finsihed

--- src/pages/Game/Tutorial.tsx
+++ src/pages/Game/Tutorial.tsx
@@ -21,27 +21,27 @@ function getText(index: number) {
         {
             guess: "woodenPlanks0",
             requiredControl: ["PickUp"],
-            text: `Welcome to the tutorial! Let's start by crafting a wooden planks, like in Minecraft. Pick up any type of log and drop it on the crafting table with the ${settings?.controls.copy} key. If you put too much materialson the crafting table, you can remove it with the ${settings?.controls.remove} key.`
+            text: `Welcome to the tutorial! Let's start by crafting a {{wooden planks}}, like in Minecraft. {{Pick up}} any type of {{log}} and {{drop it}} on the {{crafting table}} with the {{${settings?.controls.copy} key}}. If you put too much materialson the crafting table, you can {{remove it}} with the {{${settings?.controls.remove} key}}.`
         },
         {
             guess: "armorStand0",
             requiredControl: ["Copy"],
-            text: `Great! How you can see, there is no wood and log in the riddle. Let's craft an armor stand. Pick up the stick and copy it along the crafting table with the ${settings?.controls.copy} key. If you don't know how to craft an item, you can look it up in the recipe book.`
+            text: `Great! How you can see, there is no {{wood}} and {{log}} in the {{riddle}}. Let's craft an {{armor stand}}. Pick up the {{stick}} and {{copy it}} along the {{crafting table}} with the {{${settings?.controls.copy} key}}. If you don't know how to craft an item, you can look it up in the {{recipe book}}.`
         },
         {
             guess: "rail0",
             requiredControl: ["Place"],
-            text: `Good job! Now we know there are two sticks in the riddle and we know one of it's place. Let's craft a rail to see if there are any iron ingonts in the riddle. Place the materials on the crafting table with the ${settings?.controls.tableMapping.slice(0, 8).join(", ")} and ${settings?.controls.tableMapping[8]} keys.`
+            text: `Good job! Now we know there are two {{sticks}} in the {{riddle}} and we know one of it's place. Let's craft a {{rail}} to see if there are any {{iron ingonts}} in the {{riddle}}. {{Place}} the materials on the crafting table with the {{${settings?.controls.tableMapping.slice(0, 8).join(", ")}}} and {{${settings?.controls.tableMapping[8]} keys}}.`
         },
         {
             guess: "piston0",
             requiredControl: [],
-            text: `You are doing great! Now we know there are iron ingots in the riddle. Let's craft a item that contains lots of different materials... a piston! Piston is one of the best guess in the game.`
+            text: `You are doing great! Now we know there are {{iron ingots}} in the {{riddle}}. Let's craft a item that contains lots of different materials... a {{piston}}! {{Piston}} is one of the best guess in the game.`
         },
         {
             guess: "axe0",
             requiredControl: [],
-            text: `So now you might be confused. How can a riddle contain and not contain the same material at the same time? The answer is simple: in the game some recipes are in a "recipe group". For example swords. Wooden sword and iron swords has the same recipe, but with different materials, so they are in a recipe group. So the riddle can be some kind of tool. Let's try to craft an axe.`
+            text: `So now you might be confused. How can a {{riddle}} contain and not contain the same material at the same time? The answer is simple: in the game some recipes are in a "{{recipe group}}". For example {{swords}}. {{Wooden sword}} and {{iron swords}} has the same recipe, but with different materials, so they are in a {{recipe group}}. So the {{riddle}} can be some kind of tool. Let's try to craft an {{axe}}.`
         },
         {
             guess: null,
@@ -59,11 +59,11 @@ export function Tutorial(props: TutorialProps) {
 
     useEffect(() => {
         setShowTutorial(true)
-        if(currentStep) {
+        if (currentStep) {
             setTimeout(() => {
                 SoundEffect.play("allay")
                 for (let i = 0; i < currentStep.text.length; i++) {
-                    if(i % 30 == 0) {
+                    if (i % 30 == 0) {
                         setTimeout(() => {
                             SoundEffect.play("write")
                         }, i * 20);
@@ -73,14 +73,26 @@ export function Tutorial(props: TutorialProps) {
         }
     }, [props.turn])
 
+    let isHighlighted = false
+
     return currentStep && <div id="tutorial" className={showTutorial ? "showTutorial" : "hideTutorial"}>
         <div id="tutorialContent">
             <p className="tutorialText">
-                {currentStep.text.split(" ").map((word, wordIndex) => (
-                    <span
+                {currentStep.text.split(" ").map((word, wordIndex) => {
+                    const isHighlightedEnd = word.includes("}}")
+                    if (word.slice(0, 2) === "{{") {
+                        isHighlighted = true
+                    }
+                    word = word.replace("{{", "").replace("}}", "")
+
+                    const content = <span
                         key={wordIndex}
                         className="tutorialWord"
-                        style={{ display: "inline-block", whiteSpace: "nowrap" }}
+                        style={{
+                            display: "inline-block",
+                            whiteSpace: "nowrap",
+                            color: isHighlighted ? "#FFAA00" : "white"
+                        }}
                     >
                         {word.split("").map((letter, letterIndex) => {
                             const globalIndex = currentStep.text
@@ -91,7 +103,10 @@ export function Tutorial(props: TutorialProps) {
                             return (
                                 <span
                                     key={letterIndex}
-                                    style={{ animationDelay: `${globalIndex * 0.02 + 1}s` }}
+                                    style={{ 
+                                        animationDelay: `${globalIndex * 0.02 + 1}s`,
+                                        color: /[.,!?]$/.test(letter) ? "#FFFFFF" : "inherit"
+                                    }}
                                 >
                                     {letter}
                                 </span>
@@ -99,7 +114,13 @@ export function Tutorial(props: TutorialProps) {
                         })}
                         <span>&nbsp;</span>
                     </span>
-                ))}
+
+                    if (isHighlightedEnd) {
+                        isHighlighted = false
+                    }
+
+                    return content
+                })}
             </p>
             <div id="tryIt" style={{ animationDelay: `${currentStep.text.length * 0.02 + 1}s` }}>
                 <StoneButton onClick={() => { setShowTutorial(false) }}>Try It!</StoneButton>
