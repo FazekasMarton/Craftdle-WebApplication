61f51981baefec116e3091a9a9ba1fbc713f541b
FazekasMarton <fazekas.marton16@gmail.com>
2025-01-24 11:16:15 +0100

    Info bug fixed

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
-import { Navigate, RouterProvider } from "react-router"
-import { BrowserRouter, createBrowserRouter } from "react-router-dom"
+import { Navigate, Outlet, RouterProvider } from "react-router"
+import { createBrowserRouter } from "react-router-dom"
 import { MainMenu } from "./pages/MainMenu/MainMenu"
 import { Settings } from "./pages/Settings/Settings"
 import { Info } from "./components/Info"
@@ -29,107 +29,116 @@ const normalRouter = createBrowserRouter([
     {
         path: "/",
         element: <>
-            <Meta />
-            <MainMenu />
+            {isUserPlayingOnPC() ? <Info /> : null}
+            <Error />
+            <Outlet />
         </>,
-        index: true
-    },
-    {
-        path: "settings",
-        element: <>
-            <Meta
-                title="Settings"
-                description="Customize your game settings in Craftdle, including controls, audio, and icon sizes for a personalized experience on PC or mobile."
-                keywords="Craftdle, Game Settings, Controls, Audio, Icon Size, Customize Appearance, Game Preferences, Minecraft Puzzle Settings, Guideian Angel"
-            />
-            <Settings />
-        </>
-    },
-    {
-        path: "singleplayer",
-        element: <>
-            <Meta
-                title="Singleplayer"
-                description="Explore the singleplayer gamemodes in Craftdle! Test your crafting knowledge in unique puzzles designed for solo play. Play now on PC or mobile."
-                keywords="Minecraft, Wordle, Singleplayer, Craftdle Singleplayer, Solo Minecraft Puzzles, Crafting Challenges, Guideian Angel, Minecraft Wordle"
-            />
-            <Gamemodes type="singleplayer" />
-        </>
-    },
-    {
-        path: "play",
-        element: <Game />
-    },
-    {
-        path: "patchNotes",
-        element: <>
-            <Meta
-                title="Patch Notes"
-                description="Stay up-to-date with the latest updates in Craftdle! Read the patch notes to learn about new features, bug fixes, and improvements in the game."
-                keywords="Craftdle, Patch Notes, Updates, Game Changes, New Features, Bug Fixes, Game Improvements, Guideian Angel"
-            />
-            <PatchNotes />
-        </>
-    },
-    {
-        path: "credits",
-        element: <>
-            <Meta
-                title="Credits"
-                description="Meet the team behind Craftdle! Learn about the developers, artists, and contributors who helped create the game. Play now on PC or mobile."
-                keywords="Craftdle, Credits, Developers, Artists, Contributors, Game Creators, Game Designers, Guideian Angel"
-            />
-            <Credits />
-        </>
-    },
-    {
-        path: "docs",
-        element: <>
-            <Meta
-                title="Privacy Policy & Terms of Use"
-                description="Review Craftdle's Privacy Policy and Terms of Use. Learn how we protect your data and the rules you agree to when using our game."
-                keywords="Craftdle, Privacy Policy, Terms of Use, Game Rules, Data Protection, User Agreement, Minecraft Puzzle Game, Guideian Angel"
-            />
-            <Docs />
-        </>
-    },
-    {
-        path: "guide",
-        element: <>
-            <Meta
-                title="How to Play"
-                description="Learn how to play Craftdle! Read the guide to understand the game mechanics, controls, and strategies for solving puzzles. Play now on PC or mobile."
-                keywords="Craftdle, How to Play, Game Guide, Game Mechanics, Controls, Strategies, Puzzle Solving, Crafting Game, Guideian Angel"
-            />
-            <Guide />
-        </>
-    },
-    {
-        path: "collection",
-        element: <>
-            <Meta
-                title="Collection"
-                description="View your collection in Craftdle! Track your progress, view your achievements, and explore the puzzles you've completed. Play now on PC or mobile."
-                keywords="Craftdle, Collection, Achievements, Progress, Puzzles, Crafting Game, Puzzle Game, Guideian Angel"
-            />
-            <Collection />
-        </>
-    },
-    {
-        path: "stats",
-        element: <>
-            <Meta
-                title="Player Stats and Achievements - Craftdle"
-                description="Explore your personal game statistics, including your achievements, progress in different gamemodes, high scores, and overall performance in Craftdle."
-                keywords="Craftdle, Player Stats, Achievements, Game Progress, High Scores, Player Performance, Craftdle Achievements, Game Stats, Milestones, Puzzle Game Performance, Crafting Game Stats, Guideian Angel"
-            />
-            <Stats />
-        </>
-    },
-    {
-        path: "*",
-        element: <Navigate to="/" />
-    },
+        children: [
+            {
+                element: <>
+                    <Meta />
+                    <MainMenu />
+                </>,
+                index: true
+            },
+            {
+                path: "settings",
+                element: <>
+                    <Meta
+                        title="Settings"
+                        description="Customize your game settings in Craftdle, including controls, audio, and icon sizes for a personalized experience on PC or mobile."
+                        keywords="Craftdle, Game Settings, Controls, Audio, Icon Size, Customize Appearance, Game Preferences, Minecraft Puzzle Settings, Guideian Angel"
+                    />
+                    <Settings />
+                </>
+            },
+            {
+                path: "singleplayer",
+                element: <>
+                    <Meta
+                        title="Singleplayer"
+                        description="Explore the singleplayer gamemodes in Craftdle! Test your crafting knowledge in unique puzzles designed for solo play. Play now on PC or mobile."
+                        keywords="Minecraft, Wordle, Singleplayer, Craftdle Singleplayer, Solo Minecraft Puzzles, Crafting Challenges, Guideian Angel, Minecraft Wordle"
+                    />
+                    <Gamemodes type="singleplayer" />
+                </>
+            },
+            {
+                path: "play",
+                element: <Game />
+            },
+            {
+                path: "patchNotes",
+                element: <>
+                    <Meta
+                        title="Patch Notes"
+                        description="Stay up-to-date with the latest updates in Craftdle! Read the patch notes to learn about new features, bug fixes, and improvements in the game."
+                        keywords="Craftdle, Patch Notes, Updates, Game Changes, New Features, Bug Fixes, Game Improvements, Guideian Angel"
+                    />
+                    <PatchNotes />
+                </>
+            },
+            {
+                path: "credits",
+                element: <>
+                    <Meta
+                        title="Credits"
+                        description="Meet the team behind Craftdle! Learn about the developers, artists, and contributors who helped create the game. Play now on PC or mobile."
+                        keywords="Craftdle, Credits, Developers, Artists, Contributors, Game Creators, Game Designers, Guideian Angel"
+                    />
+                    <Credits />
+                </>
+            },
+            {
+                path: "docs",
+                element: <>
+                    <Meta
+                        title="Privacy Policy & Terms of Use"
+                        description="Review Craftdle's Privacy Policy and Terms of Use. Learn how we protect your data and the rules you agree to when using our game."
+                        keywords="Craftdle, Privacy Policy, Terms of Use, Game Rules, Data Protection, User Agreement, Minecraft Puzzle Game, Guideian Angel"
+                    />
+                    <Docs />
+                </>
+            },
+            {
+                path: "guide",
+                element: <>
+                    <Meta
+                        title="How to Play"
+                        description="Learn how to play Craftdle! Read the guide to understand the game mechanics, controls, and strategies for solving puzzles. Play now on PC or mobile."
+                        keywords="Craftdle, How to Play, Game Guide, Game Mechanics, Controls, Strategies, Puzzle Solving, Crafting Game, Guideian Angel"
+                    />
+                    <Guide />
+                </>
+            },
+            {
+                path: "collection",
+                element: <>
+                    <Meta
+                        title="Collection"
+                        description="View your collection in Craftdle! Track your progress, view your achievements, and explore the puzzles you've completed. Play now on PC or mobile."
+                        keywords="Craftdle, Collection, Achievements, Progress, Puzzles, Crafting Game, Puzzle Game, Guideian Angel"
+                    />
+                    <Collection />
+                </>
+            },
+            {
+                path: "stats",
+                element: <>
+                    <Meta
+                        title="Player Stats and Achievements - Craftdle"
+                        description="Explore your personal game statistics, including your achievements, progress in different gamemodes, high scores, and overall performance in Craftdle."
+                        keywords="Craftdle, Player Stats, Achievements, Game Progress, High Scores, Player Performance, Craftdle Achievements, Game Stats, Milestones, Puzzle Game Performance, Crafting Game Stats, Guideian Angel"
+                    />
+                    <Stats />
+                </>
+            },
+            {
+                path: "*",
+                element: <Navigate to="/" />
+            },
+        ]
+    }
 ])
 
 const maintenanceRouter = createBrowserRouter([
@@ -196,10 +205,6 @@ function App() {
     return (
         <>
             <RouterProvider router={maintenance.started && maintenance.countdown ? maintenanceRouter : normalRouter} />
-            {isUserPlayingOnPC() ? <Info /> : null}
-            <BrowserRouter>
-                <Error />
-            </BrowserRouter>
         </>
     )
 }
