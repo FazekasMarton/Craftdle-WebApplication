50acad65f6a0517983c66cd21384376cf347a4be
FazekasMarton <75677807+FazekasMarton@users.noreply.github.com>
2025-01-28 14:50:58 +0000

    fixes

--- src/pages/Game/Tutorial.tsx
+++ src/pages/Game/Tutorial.tsx
@@ -1,5 +1,5 @@
 import { StoneButton } from "../../components/StoneButton";
-import allay from "../../assets/imgs/others/allay.png";
+import allay from "../../assets/imgs/others/Allay.png";
 import { useEffect } from "react";
 import { SoundEffect } from "../../classes/Audio";
 import { useSelector } from "react-redux";
