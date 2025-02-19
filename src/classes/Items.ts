bfaa046ff30db3c98a479a881f75bbcab3921abb
FazekasMarton <fazekas.marton16@gmail.com>
2025-02-19 10:13:40 +0100

    image loading reworked

--- src/classes/Items.ts
+++ src/classes/Items.ts
@@ -14,103 +14,32 @@ export class Items {
     private items: { [id: string]: IItem } = {};
 
     /**
-     * Create an instance of Items.
-     * @param items - The initial items.
-     */
-    constructor(items?: Array<IItem>) {
-        if (items) {
-            this.addItems(items);
-        }
-    }
-
-    /**
-     * Add items to the collection.
-     * @param items - The items to add.
-     */
-    addItems(items: Array<IItem>) {
-        items.forEach(item => {
-            this.items[item.id] = item;
-            this.addItem(item);
-        });
-    }
-
-    /**
      * Add an item to the collection.
      * @param item - The item to add.
      */
     async addItem(item: IItem) {
-        try {
-            const cache = await caches.open('items');
-            const cachedResponse = await cache.match(item.id);
-
-            if (!cachedResponse) {
-                const response = await fetch(`http://localhost:3000/assets/items/${item.src}`);
-
-                if (!response.ok) {
-                    throw new Error(`Failed to fetch: ${response.statusText}`);
-                }
-
-                const clonedResponse = response.clone();
-                cache.put(item.id, clonedResponse);
+        if(!this.items[item.id]) {
+            const response = await fetch(`http://localhost:3000/assets/items/${item.src}`);
+    
+            if (!response.ok) {
+                return false;
             }
-        } catch (error) {
-            console.error('Error fetching item:', error);
-        }
-    }
-
-    /**
-     * Get an item by its ID.
-     * @param itemId - The ID of the item.
-     * @returns The item or undefined if not found.
-     */
-    async getItem(itemId: string) {
-        const cache = await caches.open('items');
-        const cachedResponse = await cache.match(itemId);
-
-        if (cachedResponse) {
-            const imageBlob = await cachedResponse.blob();
+    
+            const imageBlob = await response.clone().blob();
+    
             const imageUrl = URL.createObjectURL(imageBlob);
-
+    
             const img = new Image();
-            const item = this.items[itemId];
-            if (item) {
-                img.className = item.id;
-                img.alt = item.name;
-            }
+            img.className = item.id;
+            img.alt = item.name;
             img.src = imageUrl;
+            this.items[item.id] = img;
 
-            return img;
-        } else {
-            return undefined;
+            return true
         }
     }
 
-    async allImagesLoaded(): Promise<boolean> {
-        const images = await Promise.all(Object.keys(this.items).map(item => this.getItem(this.items[item].id)));
-
-        return new Promise(resolve => {
-            let loadedCount = 0;
-            const totalImages = images.length - 1;
-
-            if (totalImages === 0) {
-                resolve(true);
-                return;
-            }
-
-            images.forEach(img => {
-                if (!img) return;
-
-                img.onload = () => {
-                    loadedCount++;
-                    if (loadedCount === totalImages) {
-                        resolve(true);
-                    }
-                };
-
-                img.onerror = () => {
-                    resolve(false);
-                };
-            });
-        });
+    getItem(id: string) {
+        return this.items[id];
     }
 }
--- src/pages/Game/LoadingScreen.tsx
+++ src/pages/Game/LoadingScreen.tsx
@@ -1,6 +1,14 @@
-export function LoadingScreen() {
+interface ILoadingScreenProps {
+    total: number;
+    loaded: number;
+}
+
+export function LoadingScreen(props: ILoadingScreenProps) {
     return <div id="loadingScreen">
         <p>Loading level</p>
         <p>Building terrain</p>
+        <div id='progressBarBorder'>
+            <div id='progressBarInner' style={{width: `${props.loaded / props.total * 100}%`}}></div>
+        </div>
     </div>   
 }
