02f3c65b2591e5415a1601e540b047ed016132a3
FazekasMarton <fazekas.marton16@gmail.com>
2025-02-26 10:55:33 +0100

    disconnect fix

--- src/functions/connectSocket.ts
+++ src/functions/connectSocket.ts
@@ -6,7 +6,9 @@ import { setSocket } from "../features/socket/socketSlice";
  * Function to connect to the socket server.
  */
 export function connectSocket() {
-    store.getState().socket.socket?.disconnect()
+    const currentSocket = store.getState().socket.socket;
+    currentSocket?.off("disconnect");
+    currentSocket?.disconnect()
     const token = store.getState().user.loginToken;
     const socket: Socket = io(import.meta.env.VITE_SERVER_URL, {
         auth: {
